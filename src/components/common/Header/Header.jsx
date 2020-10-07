51e005447be891b7da5380deae79a1740a9b01ab
kimuyjoong <fr_bohaesoju@woowahan.com>
2020-10-08 08:56:04 +0900

    커밋체크 2

--- src/components/common/Header/Header.jsx
+++ src/components/common/Header/Header.jsx
@@ -3,7 +3,7 @@ import * as Styled from './style';
 
 const Header = () => (
   <Styled.Top>
-    <Styled.LeftTitle>쇼핑리스트2</Styled.LeftTitle>
+    <Styled.LeftTitle>쇼핑리스트23</Styled.LeftTitle>
   </Styled.Top>
 );
 
